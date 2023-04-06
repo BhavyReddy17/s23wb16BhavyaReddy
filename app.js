2c2d79bae499e1915f937f12695a58909177372f
Bhavya Reddy <76420040+BhavyReddy17@users.noreply.github.com>
2023-04-05 19:57:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var pancakesRouter = require('./routes/pancakes');
+var boardRouter = require('./routes/board');
 var app = express();
 
 // view engine setup
@@ -21,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/pancakes', pancakesRouter);
+app.use('/board', boardRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
